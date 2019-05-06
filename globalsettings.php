3c54f7d8809407e3f60d9ced88735913f840d8cd
chinyemi <mbutho@digitalbraintz.com>
2019-05-06 10:17:49 +0300

    rebasing tree

---
+++ globalsettings.php
@@ -0,0 +1,31 @@
+<?php 
+ define("TITLE", "GLSAngola| Dashboard");
+ define("APPLICATION_NAME", "GLS Registration System");
+ define("PROJECT_NAME", "Global Leadership Summit");
+ define("CLIENT", "GLSAngola");
+ define("CLIENT_SHORT_NAME", "GLSAngola");
+ define("CLIENT_NAME","GLSAngola | Dashboard");
+ define("CLIENT_LOGO", "http://www.angolaleadership.org/cimages/logo.jpg");
+ define("CLIENT_FLAG", "http://www.angolaleadership.org/cimages/flag.jpg");
+ define("COUNTRY", "Angola");
+ define("CURRENCY", "Kz");
+ define("TIME_ZONE","Africa/Luanda");
+ define("ZIP","244");
+ define("PREMIER_SITE", "Luanda");
+ define("GLSSSITE", "Sites in Angola");
+ define("EVENTNAME", "Global Leadership Summit");
+ define("FACEBOOK", "fb://page/glsangola");
+ define("MOBILE", "+244923336277");
+ define("ADDRESS", "P.O.BOX 29888, Luanda");
+ define("PHYSICALADDRESS", "1st Floor Freedom Plaza, Harare Ave");
+ define("PAYLATERNUMBER","+255713525707");
+ define("INSTAGRAM", "instagram://user?username=glsangola");
+ define("EMAIL", "info@angolaleadership.org");
+ define("URL","www.angolaleadership.org");
+ define("EMAIL_SENDER", "registration@angolaleadership.org");
+ define("WHATSAPP_NUMBER","+255713525707");
+ define("ALLRIGHTS", "&copy;2019.DigitalBrain Co.Ltd Ltd| ICM"); 
+ define("DIGITALBRAIN","Developed &amp; Powered By");
+ define("SENDERID","GLSAngola");
+
+  ?>
---
+++ redirect.php
@@ -0,0 +1,7 @@
+<?php
+
+include 'globalsettings.php';
+
+header("location: https://glsregistration.com/SYS/delegate_registration.php?Id=".CLIENT_SHORT_NAME);
+//exit();
+?>
