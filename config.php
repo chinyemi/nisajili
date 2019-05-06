91210df77414d0e52ff4a309269af0ac5d8078ca
chinyemi <50295350+chinyemi@users.noreply.github.com>
2019-05-06 12:42:38 +0300

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	define('DB_HOST', 'localhost');
-    define('DB_USER', 'afyapopo_glsao');
-    define('DB_PASSWORD','Gl5_Ang@2019');
-    define('DB_DATABASE', 'afyapopo_glsao');
-
-echo "some cheanges in slave branck of git";
-?>
