81077e5a486b2e9dccb16f68d7a31bad03a812e0
chinyemi <mbutho@digitalbraintz.com>
2019-05-06 17:36:54 +0300

    menu modified

--- menu.php
+++ menu.php
@@ -154,9 +154,9 @@ include('pagelinks.php');
                     <ul class="nav child_menu"> 
               
 						  
-                        <li><a href="PageSelector.php?pageID=FrameContent_RecordExpenses&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>"> Expense | Register</a></li>
+                        <li><a href="PageSelector.php?pageID=FrameContent_RecordExpenses&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>"> Revenue | Register</a></li>
           
-                         <li><a href="PageSelector.php?pageID=FrameContent_ExpensesType&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>"> Expense | Type</a></li>
+                         <li><a href="PageSelector.php?pageID=FrameContent_ExpensesType&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>"> Revenue | Type</a></li>
                       
                     </ul>
                   </li>
@@ -179,10 +179,24 @@ include('pagelinks.php');
                 </ul>
               </div>  
               
-               <div class="menu_section">
-                <h3>Budgeted RevEx</h3>
+                <div class="menu_section">
+                <h3>Budget RevEx</h3>
+                 <ul class="nav side-menu">
+                 <li><a><i class="glyphicon glyphicon-list-alt"></i>Revenue<span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu"> 
+              
+						  
+                        <li><a href="PageSelector.php?pageID=FrameContent_RecordExpenses&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>"> Revenue | Register</a></li>
+          
+                         <li><a href="PageSelector.php?pageID=FrameContent_ExpensesType&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>"> Revenue | Type</a></li>
+                      
+                    </ul>
+                  </li>
+                  
+               
+                </ul>
                 <ul class="nav side-menu">
-                 <li><a><i class="glyphicon glyphicon-tasks"></i>General Expenses<span class="fa fa-chevron-down"></span></a>
+                 <li><a><i class="glyphicon glyphicon-list-alt"></i>Expenses<span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu"> 
               
 						  
