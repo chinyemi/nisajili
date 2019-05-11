3c297ff2a0f9e2d2d4cea18aeb7d05fa66f20ead
Mat1992 <mathewmbaga@gmail.com>
2019-05-11 11:44:58 +0300

    new report files for expenses

---
+++ actualexpenses_reports.php
@@ -0,0 +1,101 @@
+<?php 
+
+require_once('header.php');
+
+?>
+<?php 
+
+$CurrYear=$_GET['Year'];
+$Mwaka=date('Y');
+$ReportID=$_GET['ReportID'];
+
+
+
+
+
+
+
+//Get site information
+
+
+$getSiteInfo=mysqli_query($conn,"SELECT * FROM `glssiteinfo` WHERE `sitename` = '".substr($ReportID, 0, -5)."'");
+
+
+
+$rowSiteInfo=mysqli_fetch_array($getSiteInfo);
+
+
+
+if ($CurrYear==$Mwaka) {
+	
+$Event = "Site Actual Expenses for ".PROJECT_NAME."(".$rowSiteInfo['sitename'].") at ".$rowSiteInfo['sitevenue']." on ".$rowSiteInfo['eventtype'];
+	
+	
+} else {
+	
+$Event = "Site Actual Expenses for ".PROJECT_NAME."(".$rowSiteInfo['sitename'].") at ".$rowSiteInfo['sitevenue']." on previous year";
+	
+	
+}
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+
+<meta name="viewport" content="initial-scale=1.0, maximum-scale=2.0">
+<link rel="stylesheet" type="text/css" href="css/jquery.dataTables.css">
+
+<script type="text/javascript" language="javascript" src="js/jquery.js"></script>
+<script type="text/javascript" language="javascript" src="js/jquery.dataTables.js"></script>
+<script type="text/javascript" language="javascript" class="init">
+
+$(document).ready(function() {
+$('#example').dataTable( {
+ "aProcessing": true,
+ "aServerSide": true,
+"ajax": "actualexpenses_reports_proc.php?Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>&ReportID=<?php echo $ReportID; ?>",
+} );
+} );
+
+</script>
+</head>
+
+<body class="dt-example">
+
+<h3><?php echo $Event;?></h3>
+<table id="example" class="display" cellspacing="0" width="100%">
+<thead>
+<tr>
+<th>expenseID</th>
+<th>Type</th>
+<th>Amount</th>
+<th>DateRecorded</th>
+<th>Description</th>
+<th>Site</th>
+<th>glsyear</th>
+
+
+</tr>
+</thead>
+</table>
+<br><br>
+
+<table id="example" class="display" cellspacing="0" width="100%">
+
+
+<th><form action="export_excel.php?ReportID=<?php echo $ReportID; ?>" method="POST">
+
+<input type="submit" name="export_excel" class="btn btn-success" value="Export all raw <?php echo substr($ReportID, 0, -5);?> data to Excel" <?php if ($_SESSION['UserLevel']!='Administrator') { ?>disabled<?php } ?> title="Only Users with Administrator role can export raw data to Excel">
+</form></th>
+<th><form action="export_excel_uploaded.php?ReportID=<?php echo $ReportID; ?>" method="POST">
+
+<input type="submit" name="export_excel" class="btn btn-success" value="Export all <?php echo substr($ReportID, 0, -5);?> bulk tickets data to Excel" <?php if ($_SESSION['UserLevel']!='Administrator') { ?>disabled<?php } ?> title="Only Users with Administrator role can export uploaded data to Excel">
+</form></th>
+</tr>
+</table>
+
+</body>
+</html>
---
+++ pagelinks.php
@@ -0,0 +1,271 @@
+<?php 
+
+$pageID=$_GET['pageID'];
+
+
+if ($pageID=='FrameContent_NoCash') {
+	
+	$iFrameLink="delegate_registration_no_pay.php";
+}
+
+if ($pageID=='FrameContent_Cash') {
+	
+	$iFrameLink="delegate_registration_manual_pay.php";
+}
+
+if ($pageID=='FrameContent_CheckIn') {
+	
+	$iFrameLink="summitchecking_ticket_proc.php";
+}
+
+if ($pageID=='FrameContent_CheckInQRCode') {
+	
+	$iFrameLink="qr/summitchecking_qrcode_proc.php";
+}
+
+if ($pageID=='FrameContent_RegistrantsReport') {
+	
+	$iFrameLink="registrantsreports_proc.php";
+}
+
+if ($pageID=='FrameContent_Car_Reports') {
+	
+	$iFrameLink="delegate_car_reports.php";
+}
+
+if ($pageID=='FrameContent_Car_CheckIn') {
+	
+	$iFrameLink="gatepasschecking_ticket_proc.php";
+}
+
+if ($pageID=='FrameContent_Speaker_Bio') {
+	
+	$iFrameLink="SpeakerBio_html.php";
+}
+
+if ($pageID=='FrameContent_Web_Video') {
+	
+	$iFrameLink="WebVideo_html.php";
+}
+
+if ($pageID=='FrameContent_Testimonies') {
+	
+	$iFrameLink="Testimonies_html.php";
+}
+
+
+
+if ($pageID=='FrameContent_Orders_Individual') {
+	
+	$iFrameLink="Process_Order_Individual_html.php";
+}
+
+if ($pageID=='FrameContent_Gen_Cert') {
+	
+	$iFrameLink="Generate_Certificates_proc.php";
+}
+if ($pageID=='SysInfo') {
+	
+	$iFrameLink="Info_html.php";
+}
+
+if ($pageID=='GLSdates') {
+	
+	$iFrameLink="Dates_html.php";
+}
+
+
+if ($pageID=='SysRates') {
+	
+	$iFrameLink="Rates_html.php";
+}
+
+
+if ($pageID=='GenerateDelRpt') {
+	
+	$iFrameLink="generatedelegaterpt_proc.php";
+}
+
+if ($pageID=='GenerateCarRpt') {
+	
+	$iFrameLink="generatecarrpt_proc.php";
+}
+
+if ($pageID=='FrameContent_SiteSeason') {
+	
+	$iFrameLink="SiteSeason_html.php";
+}
+
+if ($pageID=='textSMS') {
+	
+	$iFrameLink="CreateTextSMS.php";
+}
+
+if ($pageID=='sendSMS') {
+	
+	$iFrameLink="SendTextSMS.php";
+}
+
+if ($pageID=='sendeEmail') {
+	
+	$iFrameLink="SendEmail.php";
+}
+
+
+if ($pageID=='sentMsgs') {
+	
+	$iFrameLink="SentMesages.php";
+}
+
+
+if ($pageID=='watsapMsgs') {
+	
+	$iFrameLink="WatsapMesages.php";
+}
+
+if ($pageID=='smsBalance') {
+	
+	$iFrameLink="SmsBalance.php";
+}
+
+
+if ($pageID=='defineGroups') {
+	
+	$iFrameLink="DefineGroups.php";
+}
+
+
+if ($pageID=='assignMemberToIndividual') {
+	
+	$iFrameLink="AssignMemberToIndividual.php";
+}
+
+
+if ($pageID=='assignMemberToGroups') {
+	
+	$iFrameLink="AssignMembersToGroup.php";
+}
+
+if ($pageID=='FrameContent_RecordExpenses') {
+	
+	$iFrameLink="Expenses_html.php";
+}
+
+if ($pageID=='FrameContent_ExpensesType') {
+	
+	$iFrameLink="ExpensesType_html.php";
+}
+
+if ($pageID=='FrameContent_Element') {
+	
+	$iFrameLink="Elements_html.php";
+}
+
+if ($pageID=='FrameContent_RevenueType') {
+	
+	$iFrameLink="RevenueType_html.php";
+}
+
+
+    if ($pageID=='FrameContent_ActualRevenue') {
+	
+	$iFrameLink="ActualRevenue_html.php";
+}
+
+    if ($pageID=='FrameContent_BudgetRevenue') {
+	
+	$iFrameLink="BudgetRevenue_html.php";
+}
+
+
+if ($pageID=='FrameContent_RecordBudgetExpenses') {
+	
+	$iFrameLink="BudgetExpenses_html.php";
+	
+}
+
+if ($pageID=='FrameContent_BudgetExpensesType') {
+	
+	$iFrameLink="BudgetExpensesType_html.php";
+}
+
+//added content
+
+if ($pageID=='FrameContent_ActualRevenueReport') {
+	
+	$iFrameLink="actualrevenue_proc.php";
+}
+
+
+if ($pageID=='GenerateActualRevenueRpt') {
+	
+	$iFrameLink="generateactualrevenue_proc.php";
+}
+if ($pageID=='GenerateBudgetRevenueRpt') {
+	
+	$iFrameLink="generatebudgetrevenue_proc.php";
+}
+
+if ($pageID=='GenerateBudgetExpenseRpt') {
+	
+	$iFrameLink="generatebudgetexpense_proc.php";
+}
+
+if ($pageID=='GenerateActualExpenseRpt') {
+	
+	$iFrameLink="generateactualexpense_proc.php";
+}
+//end of added content
+if ($pageID=='FrameContent_ActualExpenseReport') {
+	
+	$iFrameLink="actualexpense_proc.php";
+}
+
+if ($pageID=='FrameContent_BudgetRevenueReport') {
+	
+	$iFrameLink="budgetrevenue_proc.php";
+}
+
+if ($pageID=='FrameContent_BudgetExpense') {
+	
+	$iFrameLink="budgetexpense_proc.php";
+}
+
+if ($pageID=='FrameContent_UserManagement') {
+	
+	$iFrameLink="UserManagement_html.php";
+}	
+	
+if ($pageID=='FrameContent_Designation') {
+	
+	$iFrameLink="Designation_html.php";
+}	
+
+if ($pageID=='FrameContent_CollectionReport') {
+	
+	$iFrameLink="collectionreports_proc.php";
+}	
+
+
+
+if ($pageID=='GenerateColRpt') {
+	
+	$iFrameLink="generatecollectionrpt_proc.php";
+}
+
+if ($pageID=='FrameContent_CheckedVehicleReport') {
+	
+	$iFrameLink="checked_in_vehiclereports_proc.php";
+}
+
+if ($pageID=='FrameContent_ActualRevenueReport') {
+	
+	$iFrameLink="actualrevenuereports_proc.php";
+}
+
+if ($pageID=='FrameContent_ActualExpensesReport') {
+	
+	$iFrameLink="actualexpensesreports_proc.php";
+}
+
+?>
