324a8599e2524c68e97af36c1c8f4e255f1a6120
Mat1992 <mathewmbaga@gmail.com>
2019-05-12 23:00:12 +0300

    mupy
    Signed-off-by: Mat1992 <mathewmbaga@gmail.com>

---
+++ actualexpensesreports_proc.php
@@ -0,0 +1,164 @@
+<?php 
+
+require_once('header.php');
+
+?>
+
+<?php
+
+if (isset($_POST["submit"])) {
+		$Season = $_POST['Season'];
+		$Site = $_POST['Site'];
+		
+
+		// Check if glsyear has been entered
+		if (!$_POST['Season']) {
+			$errSeason = 'Please select season';
+		}
+		
+		// Check if Site has been entered
+		if (!$_POST['Site']) {
+			$errSite = 'Please select site';
+		}
+		
+	
+		
+// If there are no errors, submit form
+if (!$errSeason && !$errSite) {
+	
+//$result="delegate_reports.php?Id=".$Id."&Year=".$Season."&ReportID=".$Site."_".$Season;	
+
+header("location: actualexpenses_reports.php?Id=".$Id."&Year=".$Season."&ReportID=".$Site."_".$Season);
+//exit();
+	
+	
+}
+	}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Bootstrap contact form with PHP example by BootstrapBay.com.">
+    <meta name="author" content="BootstrapBay.com">
+    <title>Generate <?php echo PROJECT_NAME;?> Certifcates</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+     <link rel="stylesheet" href="css/pikaday.css">
+    <link rel="stylesheet" href="css/bootstrap-select.css">
+    <link rel="stylesheet" href="bg.css">
+    <script>
+		function ClearFields() {
+
+     document.getElementById("fullname").value = "";
+     document.getElementById("Mobile").value = "";
+}
+	</script>	
+    
+  </head>
+  <body>
+  	
+  <h3 align="center">Generate Actual Expenses Reports</h3>
+
+  	<div class="container">
+  		<div class="row">
+  			<div class="col-md-6 col-md-offset-3">
+  				
+				<form class="form-horizontal" role="form" method="post" action="actualexpensesreports_proc.php?Id=<?php echo $Id;?>">
+				
+                   <div class="form-group">
+						<div class="col-sm-10 col-sm-offset-2">
+							<?php echo $result;?>	
+						</div>
+						
+					</div>
+                   
+
+					
+			<div class="form-group">
+		<label for="Season" class="col-sm-2 control-label">Season</label>
+		<div class="col-sm-10">
+		 <?php $getseason=mysqli_query($conn,"SELECT * FROM `siteseason` ORDER BY `Year`"); ?>
+		
+		 <select name="Season" id="Season" class="form-control">
+		  <option disabled="disabled" selected="selected">Year</option>
+		 <?php while ($rowseason=mysqli_fetch_array($getseason)) { ?>
+		   <option value="<?php echo $rowseason['Year'];?>"><?php echo $rowseason['Year'];?></option>
+          <?php }?>
+		  </select>
+		</div>
+		
+	 </div>
+					
+       
+                    
+      <div class="form-group">
+						<label for="Site" class="col-sm-2 control-label">Site</label>
+						<div class="col-sm-10">
+                 <select name="Site" id="Site" class="form-control">
+                     
+                 </select>
+						</div>
+					</div>
+					
+            			
+		<div class="form-group">
+						<div class="col-sm-10 col-sm-offset-2" align="center">
+							<input id="submit" name="submit" type="submit" value="Generate" class="btn btn-primary">
+						</div>
+					</div>
+			
+				</form> 
+			</div>
+		</div>
+	</div>   
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+     <script src="js/moment/moment.min.js"></script>
+    <script src="js/pikaday.js"></script>
+       <script type="text/javascript">
+$(document).ready(function($) {
+  var list_target_id = 'Site'; //first select list ID
+  var list_select_id = 'Season'; //second select list ID
+  var initial_target_html = '<option value="">Please select site</option>'; //Initial prompt for target select
+ 
+  $('#'+list_target_id).html(initial_target_html); //Give the target select the prompt option
+ 
+  $('#'+list_select_id).change(function(e) {
+    //Grab the chosen value on first select list change
+    var selectvalue = $(this).val();
+ 
+    //Display 'loading' status in the target select list
+    $('#'+list_target_id).html('<option value="">Please Wait...</option>');
+ 
+    if (selectvalue == "") {
+        //Display initial prompt in target select if blank value selected
+       $('#'+list_target_id).html(initial_target_html);
+    } else {
+      //Make AJAX request, using the selected value as the GET
+      $.ajax({url: 'getselectSeason.php?Id=<?php echo $Id?>&svalue='+selectvalue,
+             success: function(output) {
+                //alert(output);
+                $('#'+list_target_id).html(output);
+            },
+          error: function (xhr, ajaxOptions, thrownError) {
+            alert(xhr.status + " "+ thrownError);
+          }});
+        }
+    });
+
+});
+</script>
+<script>
+    var picker = new Pikaday({ field: document.getElementById('dob'),
+    firstDay: 1,
+        minDate: new Date(1900, 1, 1),
+        maxDate: new Date(2020, 12, 31),
+        yearRange: [1900,2020]
+        });
+</script>
+  </body>
+</html>
---
+++ menu.php
@@ -0,0 +1,340 @@
+<?php 
+
+require_once('header.php');
+
+include('pagelinks.php');
+
+?>
+             <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+              <div class="menu_section">
+                <h3>General</h3>
+                <ul class="nav side-menu">
+                  <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      
+                      
+                       
+                       
+                       <?php 
+						$getAllSeasons=mysqli_query($conn,"SELECT * FROM `siteseason`");
+						
+						while ($rowAllSeasons=mysqli_fetch_array($getAllSeasons)) {
+							
+				        if ($rowAllSeasons['SeasonStatus']=='OPEN') {
+							
+							$Season="(Current)";
+							
+							?>
+							 <li><a href="index.php?Id=<?php echo $Id;?>&Year=<?php echo $rowAllSeasons['Year'];?>"><?php echo $rowAllSeasons['Year'];?> | Season <?php echo $Season;?></a></li>
+							<?php
+						} else {
+							
+							
+						   ?>
+                       <li><a href="index.php?Id=<?php echo $Id;?>&Year=<?php echo $rowAllSeasons['Year'];?>"><?php echo $rowAllSeasons['Year'];?> | Season</a></li>
+                
+                       <?php }} ?>
+                    </ul>
+                  </li>
+                 
+                  <li><a><i class="fa fa-desktop"></i>Reg & Check-in <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                     <?php if (($_SESSION['UserLevel']=='Manager') || ($_SESSION['UserLevel']=='Administrator')) { ?>
+                      <li><a href="PageSelector.php?pageID=FrameContent_NoCash&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Registration | No Cash</a></li>
+                      <li><a href="PageSelector.php?pageID=FrameContent_Cash&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Registration | Individual Payment</a></li>
+                     
+                    
+                     <?php } ?>
+                 
+                      <li><a href="PageSelector.php?pageID=FrameContent_CheckIn&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Check in | TicketNo</a></li>
+                      <li><a href="PageSelector.php?pageID=FrameContent_CheckInQRCode&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Check in | QR Codes</a></li>
+                       <li><a href="PageSelector.php?pageID=FrameContent_Car_CheckIn&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Check-in | Vehicles</a></li>
+                      
+                    </ul>
+                  </li>
+                  
+                  
+                  <li><a><i class="fa fa-credit-card"></i>Order Processing <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                     <?php if (($_SESSION['UserLevel']=='Manager') || ($_SESSION['UserLevel']=='Administrator')) { ?>
+                  
+                      <li><a href="PageSelector.php?pageID=FrameContent_Orders_Individual&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Process Orders | Individual</a></li>
+                      
+                         <li><a href="PageSelector.php?Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Process Orders | Group</a></li>
+                    
+                     <?php } ?>
+                 
+                      
+                    </ul>
+                  </li>
+                  
+                 
+                    </ul>
+                </li>
+                </ul>
+              </div>
+              
+              
+                  <div class="menu_section">
+                <h3>Reports</h3>
+                <ul class="nav side-menu">
+                 <li><a><i class="glyphicon glyphicon-th"></i>Dynamic Reports<span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu"> 
+                    
+                     
+						  
+                        <li><a href="PageSelector.php?pageID=FrameContent_RegistrantsReport&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>"> Registrants | Delegate</a></li>
+          
+                       <li><a href="PageSelector.php?pageID=FrameContent_CollectionReport&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>"> Collections | Summary</a></li>
+                        <li><a href="PageSelector.php?pageID=FrameContent_ActualRevenueReport&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Actual | Revenue</a></li>
+                        <li><a href="PageSelector.php?pageID=FrameContent_ActualExpensesReport&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Actual | Expenses</a></li>
+                        <li><a href="PageSelector.php?pageID=FrameContent_BudgetRevenueReport&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Budgeted | Revenue</a></li>
+                        <li><a href="PageSelector.php?pageID=FrameContent_BudgetExpensesReport&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Budgeted | Expenses</a></li>
+                        <li><a href="PageSelector.php?pageID=FrameContent_CheckedVehicleReport&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Checked-in | Vehicle</a></li>
+                        
+                        <li><a href="PageSelector.php?pageID=sentMsgs&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Message | Sents</a></li>
+                    
+                  
+                      
+                    </ul>
+                  </li>
+                  
+                 
+                                  
+                
+                </ul>
+              </div> 
+              
+              <div class="menu_section">
+                <h3>Communication</h3>
+                <ul class="nav side-menu">
+                 <li><a><i class="glyphicon glyphicon-phone"></i>Messaging Management <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu"> 
+                     <?php if ($_SESSION['UserLevel']=='Administrator') { ?> 
+                     
+						   <li><a href="PageSelector.php?pageID=defineGroups&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Create | Group</a></li>
+                      
+                        <li><a href="PageSelector.php?pageID=assignMemberToGroups&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Assign | Group</a></li>
+                       
+                       <li><a href="PageSelector.php?pageID=assignMemberToIndividual&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Assign | Individual</a></li>
+                      
+                        
+                       <li><a href="PageSelector.php?pageID=textSMS&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Create | SMS</a></li>
+                       
+                       <li><a href="PageSelector.php?pageID=sendSMS&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Send | SMS</a></li>
+                       
+                       
+                       
+                        <li><a href="PageSelector.php?pageID=sendeEmail&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Send | Email</a></li>
+                       
+                       
+                      <li><a href="PageSelector.php?pageID=watsapMsgs&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Send | WhatsApp</a></li>
+                      
+                       <li><a href="PageSelector.php?pageID=smsBalance&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Recharge | Messaging</a></li>
+                    
+                        
+                      <?php } ?>
+                      
+                    </ul>
+                  </li>
+                  
+            
+                
+                </ul>
+              </div> 
+                  
+            <div class="menu_section">
+                <h3>Financials</h3>
+                 <ul class="nav side-menu">
+                 <li><a><i class="glyphicon glyphicon-tasks"></i>Actual Revenue<span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu"> 
+              
+						  
+                       
+                        <li><a href="PageSelector.php?pageID=FrameContent_ActualRevenue&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>"> Revenue | Register</a></li>
+          
+                        
+                        
+                      
+                    </ul>
+                  </li>
+                  
+               
+                </ul>
+                <ul class="nav side-menu">
+                 <li><a><i class="glyphicon glyphicon-tasks"></i>Actual Expenses<span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu"> 
+              
+						  
+                        <li><a href="PageSelector.php?pageID=FrameContent_RecordExpenses&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>"> Expense | Register</a></li>
+          
+                         
+                      
+                    </ul>
+                  </li>
+                            
+                </ul>
+                <ul class="nav side-menu">
+                 <li><a><i class="glyphicon glyphicon-list-alt"></i>Budget Revenue<span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu"> 
+              
+						  
+                       <li><a href="PageSelector.php?pageID=FrameContent_BudgetRevenue&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>"> Revenue | Register</a></li>
+          
+                        <li><a href="PageSelector.php?pageID=FrameContent_RevenueType&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>"> Revenue | Type</a></li>
+                      
+                    </ul>
+                  </li>
+                  
+               
+                </ul>
+                <ul class="nav side-menu">
+                 <li><a><i class="glyphicon glyphicon-list-alt"></i>Budget Expenses<span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu"> 
+              
+						  
+                          
+                        <li><a href="PageSelector.php?pageID=FrameContent_RecordBudgetExpenses&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>"> Budget Expense | Register</a></li>
+          
+                         <li><a href="PageSelector.php?pageID=FrameContent_BudgetExpensesType&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Budget Expense | Type</a></li>
+                      
+                      
+                    </ul>
+                  </li>
+                  
+               
+                </ul>
+              </div>  
+              
+              
+            
+               <div class="menu_section">
+                <h3>Website Administration</h3>
+                <ul class="nav side-menu">
+                 <li><a><i class="glyphicon glyphicon-link"></i>Contents Management <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu"> 
+                     <?php if ($_SESSION['UserLevel']=='Administrator') { ?> 
+                      <li><a href="PageSelector.php?pageID=FrameContent_Speaker_Bio&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Speaker | Biography</a></li>
+                      
+                      <li><a href="PageSelector.php?pageID=FrameContent_Web_Video&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Web | Videos</a></li>
+                      
+                      <li><a href="PageSelector.php?pageID=FrameContent_Testimonies&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Testimonies | Feedback</a></li>
+                      
+                      <li><a href="PageSelector.php?pageID=FrameContent_Element&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Web | Element</a></li>
+                    
+                        
+                      <?php } ?>
+                      
+                    </ul>
+                  </li>
+                  
+             
+                </ul>
+              </div>
+              
+            
+             
+               <div class="menu_section">
+                <h3>System Administration</h3>
+                <ul class="nav side-menu">
+                 <li><a><i class="glyphicon glyphicon-certificate"></i> Manage | Certificate <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu"> 
+                     <?php if ($_SESSION['UserLevel']=='Administrator') { ?> 
+                   
+                      <li><a href="Generate_Certificates.php?pageID=FrameContent_Gen_Cert&Id=<?php echo $Id;?>&Year=<?php echo '2019';?>">Certificate | Process</a></li>
+  
+                       <li><a href="Process_Payment_CRUD.php?Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Certificate | Payment</a></li>
+                  
+                      
+                      <?php } ?>
+                      
+                    </ul>
+                  </li>
+                  
+                  <li><a><i class="glyphicon glyphicon-map-marker"></i> Site | Event <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu"> 
+                     <?php if ($_SESSION['UserLevel']=='Administrator') { ?> 
+          
+                      <li><a href="PageSelector.php?pageID=SysInfo&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Event | Details</a></li>
+          
+                        <li><a href="PageSelector.php?pageID=SysRates&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Registration | Fees</a></li>
+                        
+                        <li><a href="PageSelector.php?pageID=GLSdates&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Event | Dates</a></li>
+                       
+                       <li><a href="PageSelector.php?pageID=GLSPackage&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Package | Management</a></li>
+                      
+                      <?php } ?>
+                      
+                    </ul>
+                  </li>
+                 
+                    <li><a><i class="glyphicon glyphicon-user"></i> System | Users <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu"> 
+                     <?php if ($_SESSION['UserLevel']=='Administrator') { ?> 
+                      
+                      
+                     <li><a href="PageSelector.php?pageID=FrameContent_UserManagement&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>"> User | Management</a></li>
+                     <li><a href="PageSelector.php?pageID=FrameContent_Designation&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>"> Designation | Management</a></li>
+                     
+                      
+                      <?php } ?>
+                      
+                    </ul>
+                  </li>  
+                  <li><a><i class="glyphicon glyphicon-eye-open"></i> Create | Views <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu"> 
+                     <?php if ($_SESSION['UserLevel']=='Administrator') { ?> 
+                     <li><a href="PageSelector.php?pageID=GenerateDelRpt&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Views | Delegates</a></li>
+                     <li><a href="PageSelector.php?pageID=GenerateColRpt&Id=<?php echo $Id;?>&Year=<?php echo $CurrYear;?>">Views | Collections</a></li>  
+                        <li><a href="PageSelector.php?pageID=GenerateCarRpt&Id=<?php echo $Id;?>&Year=<?php echo '2019';?>">Views | Car Parking</a></li>
+                     
+                       <li><a href="PageSelector.php?pageID=GenerateBudgetRevenueRpt&Id=<?php echo $Id; ?>&Year=<?php echo '2019'; ?>">Views | Budget Revenue</a></li>
+                        <li><a href="PageSelector.php?pageID=GenerateActualRevenueRpt&Id=<?php echo $Id; ?>&Year=<?php echo '2019'; ?>">Views | Actual Revenue</a></li>
+                        <li><a href="PageSelector.php?pageID=GenerateBudgetExpenseRpt&Id=<?php echo $Id; ?>&Year=<?php echo '2019'; ?>">Views | Budget Expenses</a></li>
+                        <li><a href="PageSelector.php?pageID=GenerateActualExpenseRpt&Id=<?php echo $Id; ?>&Year=<?php echo '2019'; ?>">Views | Actual Expenses</a></li>
+                     
+                      
+                      <?php } ?>
+                    
+                    </ul>
+                  </li>   
+                  
+               
+                  
+                  <li><a><i class="glyphicon glyphicon-calendar"></i> Set up | Season  <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu"> 
+                     <?php if ($_SESSION['UserLevel']=='Administrator') { ?> 
+                   
+                      
+                      <li><a href="PageSelector.php?pageID=FrameContent_SiteSeason&Id=<?php echo $Id;?>">Season | Management</a></li>
+                    
+                      <?php } ?>
+                      
+                    </ul>
+                  </li>   
+                  
+                <li><a><i class="glyphicon glyphicon-credit-card"></i> Online | Payment <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu"> 
+                     <?php if ($_SESSION['UserLevel']=='Administrator') { ?> 
+                      
+                 
+      
+                      <li><a href="paymentgateway.php?Id=<?php echo $Id;?>&Year=<?php echo '2019';?>">Payments | Gateway</a></li>
+                      
+                      <?php } ?>
+                      
+                    </ul>
+                  </li>    
+                  
+                 <li><a><i class="glyphicon glyphicon-wrench"></i> System | Support <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu"> 
+                     
+                      <li><a href="https://www.nisajili.com/support/" target="_blank">Online Support</a></li>
+                    </ul>
+                  </li>                                                                                                                                                                                 
+                
+                </ul>
+              </div>
+              
+
+            </div>
